a0abfe02fc04d334974ea0c58c0e2da673430fb9
firm4458 <pattanachai_ac133@yahoo.com>
2020-05-02 15:51:50 +0700

    new picture loading system

--- src/logic/util/ResourceManager.java
+++ src/logic/util/ResourceManager.java
@@ -12,7 +12,23 @@ import javafx.scene.media.MediaPlayer;
 public class ResourceManager {
 	private ResourceManager() {}
 	private static TreeMap<String,InputStream> map = new TreeMap<String,InputStream>();
+	private static TreeMap<String,Image> imageMap = new TreeMap<String, Image>();
 	private static TreeMap<String,Media> soundMap = new TreeMap<String,Media>();
+	
+	static {
+		addImage("bullet", "img/bullet1.png", 50, 50, true);
+	}
+	
+	public static void addImage(String imageName, String path, double width, double height, boolean perserveRatio) {
+		Image img = new Image(ClassLoader.getSystemResource(path).toString(),width,height,perserveRatio,true);
+		imageMap.put(imageName, img);
+	}
+	
+	public static Image getImage(String imageName) {
+		return imageMap.get(imageName);
+	}
+	
+	@Deprecated
 	public static Image getImage(String url,double width, double height) {
 		if(!map.containsKey(url)) {
 			map.put(url, ClassLoader.getSystemResourceAsStream(url));
